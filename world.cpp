2a31d1dd00293059775f961d04c6db419f44d227
skyoker <johanskajaa@gmail.com>
2025-10-28 19:53:02 +0000

    BIG commit

---
+++ world.cpp
@@ -0,0 +1,34 @@
+#include "world.hpp"
+
+
+std::map<std::string, std::map<std::string, std::string>> World::LoadChunk(int xpos, int ypos) {
+    std::string chunkfilename = "ch" + std::to_string(xpos) + "x" + std::to_string(ypos) + "y.json";
+    fs::path chunkpath = path_to_world / chunkfilename;
+
+    std::ifstream file(chunkpath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open chunk: " << chunkpath << "\n";
+        return {};
+    }
+
+    json j;
+    file >> j;  // parse the JSON
+    file.close();
+
+    std::map<std::string, std::map<std::string, std::string>> chunkData;
+
+    for (auto& [tileKey, tileValue] : j.items()) {
+        std::map<std::string, std::string> tileMap;
+        for (auto& [k, v] : tileValue.items()) {
+            tileMap[k] = v.get<std::string>();
+        }
+        chunkData[tileKey] = tileMap;
+    }
+
+    return chunkData;
+}
+
+std::map<std::string, std::string> GetTile(int xpos, int ypos, const std::map<std::string, std::map<std::string, std::string>>& chunk) {
+
+    
+}
--- world.hpp
+++ world.hpp
@@ -1,3 +1,21 @@
 #pragma once
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <iostream>
 
-struct World {}
+using json = nlohmann::json;
+
+
+namespace fs = std::filesystem;
+
+struct World {
+    fs::path path_to_world; 
+
+    World(const fs::path& Worldpath) : path_to_world(Worldpath) {}
+
+    std::map<std::string, std::map<std::string, std::string>> LoadChunk(int xpos, int ypos);
+    std::map<std::string, std::string> GetTile(int xpos, int ypos, const std::map<std::string, std::map<std::string, std::string>>& chunk);
+};
